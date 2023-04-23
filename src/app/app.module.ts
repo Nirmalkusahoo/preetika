07790a8ce07c7d1e0ef933d4d5627e369093387e
i537736 <nirmal.kumar.sahoo@sap.com>
2023-04-23 22:00:45 +0530

    intial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { ComingSoonComponent } from './coming-soon/coming-soon.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    ComingSoonComponent
   ],
   imports: [
     BrowserModule,
